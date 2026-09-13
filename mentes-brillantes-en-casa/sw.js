d474528fe55da46374dbb5f5caace6cf25ea0b67
Julián Medina <42697034+JulianDM1995@users.noreply.github.com>
2026-09-12 21:16:33 -0500

    Actualizar caché para mostrar propósitos de actividades

--- mentes-brillantes-en-casa/sw.js
+++ mentes-brillantes-en-casa/sw.js
@@ -1,4 +1,4 @@
-const CACHE='mb-en-casa-v3';
+const CACHE='mb-en-casa-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./logo.svg','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
