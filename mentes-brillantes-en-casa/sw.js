2f5d6763a5ad0a5cf87c34838b9fd9e07e7b6fe2
Julián Medina <42697034+JulianDM1995@users.noreply.github.com>
2026-09-13 09:03:23 -0500

    Simplificar actividades a estados casa y jardín

--- mentes-brillantes-en-casa/sw.js
+++ mentes-brillantes-en-casa/sw.js
@@ -1,4 +1,4 @@
-const CACHE='mb-en-casa-v8';
+const CACHE='mb-en-casa-v9';
 const ASSETS=['./','./index.html','./descubro-quien-soy.html','./animales-grandes-pequenos.html','./pelotitas-amigas.html','./manifest.webmanifest','./logo.svg','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
